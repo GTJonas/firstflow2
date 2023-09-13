693a58184c1ece6692ed7ad5c9a2ff164730755e
GTJonas <143603242+GTJonas@users.noreply.github.com>
2023-09-13 09:46:05 +0200

    Delete frontend directory

--- frontend/src/Components/Scrollbar.tsx
+++
@@ -1,7 +0,0 @@
-import "../components/Style-modules/Scrollbar-style-module.css";
-
-function Scrollbar() {
-  return <div className="Mobile--Scroll">{/* Scrollbar content */}</div>;
-}
-
-export default Scrollbar;
--- frontend/src/api/getAuthHeaders.tsx
+++
@@ -1,9 +0,0 @@
-const getAuthHeaders = () => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    return { Authorization: `Bearer ${token}` };
-  }
-  return {};
-};
-
-export default getAuthHeaders;
